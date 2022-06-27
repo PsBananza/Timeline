d0b7da129cc5297054ee892d27f048b36dc5ea6f
dspicin <dspicin@dunice.net>
2022-06-27 09:55:13 +0300

    Created project

---
+++ src/main/java/com/dmitry/NewsClient/NewsClientApplication.java
@@ -0,0 +1,13 @@
+package com.dmitry.NewsClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmitry/NewsClient/NewsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmitry.NewsClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
